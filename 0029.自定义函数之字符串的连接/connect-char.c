8d4f1abd34b3a228644402e94826d3234f6d9754
173****5206 <wangcheng147@outlook.com>
2024-06-01 01:50:29 +0000

    1

--- 0029.自定义函数之字符串的连接/connect-char.c
+++ 0029.自定义函数之字符串的连接/connect-char.c
@@ -8,9 +8,9 @@ int main(){
     fgets(&int_data[1][0],1000,stdin);  //stdin:standard input 标准输入，这里代表从键盘输入
     count[0] = (int)strlen(&int_data[0][0]);
     count[1] = (int)strlen(&int_data[1][0]);
-    int_data[0][count[0]-1] = '\0';
+    int_data[0][count[0]-1] = '\0';  
     int_data[1][count[1]-1] = '\0';
-    
+
     // gets(int_data[0]);   
     // gets(int_data[1]);
 
