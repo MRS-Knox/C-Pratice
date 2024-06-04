838e14584e39521adddfaf9aad394b1ec7692535
173****5206 <wangcheng147@outlook.com>
2024-06-04 06:30:29 +0000

    none

--- 0031.自定义函数之数字分离/seperation-number.c
+++ 0031.自定义函数之数字分离/seperation-number.c
@@ -9,5 +9,5 @@ int main(){
     out_data[2] = int_data%100/10;
     out_data[3] = int_data%10;
     printf("%d %d %d %d",out_data[0],out_data[1],out_data[2],out_data[3]);
-    return 0;
+    return 0;  
 }
