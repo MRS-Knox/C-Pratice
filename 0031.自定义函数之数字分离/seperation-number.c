7214198b747c651bc0bf7fecd45e848c5ea44c56
173****5206 <wangcheng147@outlook.com>
2024-06-04 06:29:32 +0000

    1

---
+++ 0031.自定义函数之数字分离/seperation-number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){  
+    int int_data = 0;
+    int out_data[4] = {0};  //千 百 十 个
+    scanf("%d",&int_data);
+    out_data[0] = int_data/1000;
+    out_data[1] = int_data%1000/100;
+    out_data[2] = int_data%100/10;
+    out_data[3] = int_data%10;
+    printf("%d %d %d %d",out_data[0],out_data[1],out_data[2],out_data[3]);
+    return 0;
+}
