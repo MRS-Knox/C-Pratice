762505a05b3b01ce8b93528cb6fb1aa48d0b5f78
173****5206 <wangcheng147@outlook.com>
2024-06-12 05:46:18 +0000

    内含宏定义说明

--- 0035.宏定义之闰年的判断/ajust-year.c
+++ 0035.宏定义之闰年的判断/ajust-year.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 //闰年的判断条件：1.普通年份能被4整除而不能被100整除    2.世纪年份能被400整除
 
-#define LEAP_YEAR(a,b)    b = Ajust_LeapYear(a)
+#define LEAP_YEAR(a)    Ajust_LeapYear(a)   //相当于    LEAP_YEAR(a) = Ajust_LeapYear(a)
 
 int Ajust_LeapYear(int year){
     if(year % 4 == 0){
@@ -21,9 +21,7 @@ int Ajust_LeapYear(int year){
 int main(){
     int int_data = 0;  
     char out_data[2] = {'L','N'};
-    int out_count = 0;
     scanf("%d",&int_data);
-    LEAP_YEAR(int_data,out_count);
-    printf("%c",out_data[out_count]);
+    printf("%c",out_data[LEAP_YEAR(int_data)]);
     return 0;
 } 
